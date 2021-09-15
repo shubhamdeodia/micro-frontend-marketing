a25b57cfba4567cc6b5e7cc2137f1d96d64ca3fe
shubhamdeodia <shub.deodia@gmail.com>
2021-09-15 12:19:09 +0530

    handling routing

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if(onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history}/>, el);
+
+  const onParentNavigate = ({ pathname: nextPathName }) => {
+    const {pathname} = history.location;
+
+    if(pathname !== nextPathName){
+      history.push(nextPathName)
+    }
+  }
+
+  return {
+    onParentNavigate
+  }
+
 };
+
+
+
 
 // If we are in development and in isolation,
 // call mount immediately
@@ -13,7 +37,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
