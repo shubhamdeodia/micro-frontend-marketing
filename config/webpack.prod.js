2fa813b927f0bc4f401cc015c92543930929a1b7
shubhamdeodia <shubh.deodia@gmail.com>
2022-04-14 12:10:38 +0530

    ci: updated build dir and auth url

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,19 +1,23 @@
-const { merge } = require("webpack-merge");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const packageJson = require("../package.json");
-const commonConfig = require("./webpack.common");
+const { merge } = require('webpack-merge');
+const path = require('path');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+const commonConfig = require('./webpack.common');
+
+const BUILD_DIR = path.resolve(__dirname, '../build');
 
 const prodConfig = {
-  mode: "production",
+  mode: 'production',
   output: {
-    filename: "[name].[contenthash].js",
+    path: BUILD_DIR,
+    filename: '[name].[contenthash].js',
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "marketing",
-      filename: "remoteEntry.js",
+      name: 'marketing',
+      filename: 'remoteEntry.js',
       exposes: {
-        "./MarketingApp": "./src/App",
+        './MarketingApp': './src/App',
       },
       shared: packageJson.dependencies,
     }),
